97649c35a58bd48a155a2aac67fe9e2b981a4883
Guilherme Moura <mouraggui@gmail.com>
2023-08-19 17:32:09 -0300

    feat: add portifolio section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,24 +8,52 @@ import {
   Title,
   TextContent,
   LinkContainer,
+  ToolContainer,
+  ToolTitle,
+  ToolList,
+  ToolListItem,
+  ToolListSeparator,
 } from './styles';
 
-const Main = () => {
-  const linkList = [
-    {
-      icon: <Github />,
-      link: 'https://github.com/glhrmoura',
-    },
-    {
-      icon: <CodePen />,
-      link: 'https://codepen.io/glhrmoura',
-    },
-    {
-      icon: <Mail />,
-      link: 'mailto:mouraggui@gmail.com',
-    },
-  ];
+const linkList = [
+  {
+    icon: <Github />,
+    link: 'https://github.com/glhrmoura',
+  },
+  {
+    icon: <CodePen />,
+    link: 'https://codepen.io/glhrmoura',
+  },
+  {
+    icon: <Mail />,
+    link: 'mailto:mouraggui@gmail.com',
+  },
+];
 
+const toolList = [
+  {
+    title: 'Vue Calendar',
+    toolLink: 'https://www.npmjs.com/package/@glhrmoura/vue-calendar',
+    githubLink: 'https://github.com/glhrmoura/vue-calendar',
+  },
+  {
+    title: 'Imports Sorter',
+    toolLink: 'https://marketplace.visualstudio.com/items?itemName=glhrmoura.imports-sorter',
+    githubLink: 'https://github.com/glhrmoura/imports-sorter',
+  },
+  {
+    title: 'Coins',
+    toolLink: 'https://chrome.google.com/webstore/detail/coins/meebfpmdedodccopjbkcihiecpmiljml',
+    githubLink: 'https://github.com/glhrmoura/coins',
+  },
+  {
+    title: 'React Conditional',
+    toolLink: 'https://www.npmjs.com/package/@glhrmoura/react-conditional',
+    githubLink: 'https://github.com/glhrmoura/react-conditional',
+  },
+];
+
+const Main = () => {
   return (
     <Container>
       <Content>
@@ -34,6 +62,24 @@ const Main = () => {
           My name is Guilherme Moura, from Brazil. I have more than 7 years of experience with software development,
           always looking to deepen my knowledge and develop new things.
         </TextContent>
+        <ToolContainer>
+          <ToolTitle>Portfolio</ToolTitle>
+          <ToolList>
+            {toolList.map((tool, index) => (
+              <ToolListItem key={index}>
+                <a href={tool.toolLink} target="_blank" rel="noreferrer">
+                  {tool.title}
+                </a>
+                <ToolListSeparator>
+                  |
+                </ToolListSeparator>
+                <a href={tool.githubLink} target="_blank" rel="noreferrer">
+                  Github
+                </a>
+              </ToolListItem>
+            ))}
+          </ToolList>
+        </ToolContainer>
         <LinkContainer>
           {linkList.map((contact, index) => (
             <a key={index} href={contact.link} target="_blank" rel="noreferrer">
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -15,15 +15,40 @@ export const Content = styled.div`
 
 export const Title = styled.h1`
   font-size: 32px;
-  margin: 0px 0px 24px 0px;
 `;
 
 export const TextContent = styled.p`
-  line-height: 24px;
-  margin: 0px 0px 24px 0px;
+  margin-top: 32px;
 `;
 
 export const LinkContainer = styled.div`
   display: flex;
   gap: 16px;
+  margin-top: 32px;
+`;
+
+export const ToolContainer = styled.div`
+  margin-top: 32px;
+`;
+
+export const ToolTitle = styled.h2`
+  font-size: 22px;
+`;
+
+export const ToolList = styled.ul`
+  padding-left: 16px;
+  margin: 0px;
+  margin-top: 8px;
+`;
+
+export const ToolListItem = styled.li`
+  font-weight: 300;
+  padding: 2px 0px 2px 8px;
+`;
+
+export const ToolListSeparator = styled.span`
+  position: relative;
+  display: inline-block;
+  margin: 0px 8px;
+  font-weight: 200;
 `;
