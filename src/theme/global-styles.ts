c948533fd58b35be206de5346a51b2d15edbda78
Guilherme Moura <mouraggui@gmail.com>
2023-08-19 17:31:41 -0300

    feat: add new font weight

--- src/theme/global-styles.ts
+++ src/theme/global-styles.ts
@@ -1,16 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
-import OktaNeueBold from '../assets/fonts/OktaNeue-Bold.ttf';
 import OktaNeueLight from '../assets/fonts/OktaNeue-Light.ttf';
+import OktaNeueNormal from '../assets/fonts/OktaNeue-Normal.ttf';
 import OktaNeueMedium from '../assets/fonts/OktaNeue-Medium.ttf';
 import OktaNeueRegular from '../assets/fonts/OktaNeue-Regular.ttf';
 import OktaNeueSemiBold from '../assets/fonts/OktaNeue-SemiBold.ttf';
+import OktaNeueBold from '../assets/fonts/OktaNeue-Bold.ttf';
 
 export default createGlobalStyle`
   @font-face {
     font-family: 'Okta Neue';
     font-weight: 200;
     src: url(${OktaNeueLight})  format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Okta Neue';
+    font-weight: 300;
+    src: url(${OktaNeueNormal})  format('truetype');
   }
 
   @font-face {
@@ -40,10 +47,25 @@ export default createGlobalStyle`
   * {
     color: #D0D0D1;
     font-family: 'Okta Neue', sans-serif;
-    letter-spacing: 1px;
+    letter-spacing: 0.5px;
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    margin: 0px;
   }
 
   body {
     margin: 0px;
+  }
+
+  a {
+    color: #A1ECF7;
+    text-decoration: none;
+    text-underline-offset: 4px;
+
+    &:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 1px;
+    }
   }
 `;
