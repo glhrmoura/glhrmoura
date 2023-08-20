6aef7d8901dd80b303ccd45893f7e6cfa3d23cbc
Guilherme Moura <mouraggui@gmail.com>
2023-08-20 11:22:53 -0300

    refactor: using vite

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import Home from './pages/Home';
+
+import GlobalStyles from './theme/global-styles';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <Home />
+  </React.StrictMode>,
+);
