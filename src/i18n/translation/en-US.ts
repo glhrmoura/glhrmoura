38ca197a6753569dccf498b1e12b108488109c05
Guilherme Moura <mouraggui@gmail.com>
2023-12-01 14:42:39 -0300

    fix: title

--- src/i18n/translation/en-US.ts
+++ src/i18n/translation/en-US.ts
@@ -1,5 +1,5 @@
 export default {
   title: 'Hi there',
   content: 'With more than 7 years of experience in software development, I always seek to deepen my knowledge and develop new things.',
-  portifolio: 'Portfolio',
+  portifolio: 'Personal Projects',
 };
--- src/i18n/translation/pt-BR.ts
+++ src/i18n/translation/pt-BR.ts
@@ -2,5 +2,5 @@ export default {
   title: 'Olá',
   content:
     'Com mais de 7 anos de experiência em desenvolvimento de software, sempre busco aprofundar meus conhecimentos e desenvolver coisas novas.',
-  portifolio: 'Portfólio',
+  portifolio: 'Projetos Pessoais',
 };
